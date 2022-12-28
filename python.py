a679733ab2fffcd9004c1d0c47a3ba06666f5bfb
Yaroslav Gritsaev <yaroslavgticaev@gmail.com>
2022-12-28 20:02:15 +0600

    delet old db

--- python.py
+++ python.py
@@ -3,14 +3,16 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QHeaderView
 
 
 class DBSample(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main_window.ui', self)
-        self.connection = sqlite3.connect("films.sqlite")
+        self.connection = sqlite3.connect("films.db")
+        self.types = {'Режиссёр': 'director', 'Жанр': 'Genre', 'Год': 'Year', 'Название': 'Name'}
+        self.btn_was_clicked = False
         self.btn.clicked.connect(self.select_data)
         self.select_data()
 
@@ -18,17 +20,33 @@ class DBSample(QMainWindow):
         cur = self.connection.cursor()
         self.tableWidget.setColumnCount(7)
         self.tableWidget.setRowCount(0)
-        print(cur.description)
+        cb_contant = self.types[self.comboBox.currentText()]
+        search = str(self.lineEdit.text().capitalize())
+        if self.btn_was_clicked:
+            res = cur.execute(""" 
+                            SELECT * FROM films
+                            WHERE {} LIKE '%{}%'
+                            """.format(cb_contant, search)).fetchall()
+        else:
+            res = cur.execute("""
+                            SELECT * FROM films
+                            """)
         names = list(map(lambda x: x[0], cur.description))
-        res_2 = cur.execute(f""" SELECT * FROM films""").fetchall()
-        print(res_2)
-        for i, row in enumerate(res_2):
+        print(res)
+        for i, row in enumerate(res):
             self.tableWidget.setRowCount(
                 self.tableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
                 self.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(elem)))
-        # self.tableWidget.setHorizontalHeaderLabels(names)
+        self.tableWidget.setHorizontalHeaderLabels(names)
+        stylesheet = "::section{Background-color:rgb(175, 175, 175);border-radius:14px;}"
+        self.tableWidget.horizontalHeader().setStyleSheet(stylesheet)
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.tableWidget.verticalHeader().hide()
+        self.btn_was_clicked = True
+
+        
 
         
 
